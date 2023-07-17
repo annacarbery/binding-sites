61a298ebe87dedb2e38b39d9e951343e04a82efe
Anna Carbery <44606864+annacarbery@users.noreply.github.com>
2023-07-17 12:04:30 +0300

    Update run_all.sh

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,7 +1,4 @@
-source /data/xchem-fragalysis/tyt15771/miniconda3/bin/activate esm
-cd /data/xchem-fragalysis/tyt15771/projects/frag/
-python binding-sites/src/predict_residues.py -t $1 -num_models $2 -af2 0
-python binding-sites/src/predict_residues.py -t $1 -num_models $2 -af2 1
-source /data/xchem-fragalysis/tyt15771/miniconda3/bin/activate pymol
-python binding-sites/src/predict_centres.py -t $1 -num_models $2 -af2 0
-python binding-sites/src/predict_centres.py -t $1 -num_models $2 -af2 1
+conda activate esm_env
+python src/predict_residues.py -t $1
+conda activate pymol_env
+python src/predict_centres.py -t $1
